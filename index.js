a862d4832ef2c7e163d005c660270966242050d9
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-29 15:43:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(driver => driver.startsWith(string))
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(driver => driver.name === string)
+}
